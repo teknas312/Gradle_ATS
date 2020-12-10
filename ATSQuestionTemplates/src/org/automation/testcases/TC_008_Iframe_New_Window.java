d9e759d66f77fac564b8f4fd7a2a0a0ce75c9ad7
sdalvi <sanket.dalvi@altres.in>
2020-12-10 10:17:42 +0530

    Made changes in TC_8 and properties file

--- ATSQuestionTemplates/src/org/automation/testcases/TC_008_Iframe_New_Window.java
+++ ATSQuestionTemplates/src/org/automation/testcases/TC_008_Iframe_New_Window.java
@@ -2,6 +2,7 @@ package org.automation.testcases;
 
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.Random;
 import java.util.Set;
 
 import org.automation.Reporting.ExtentReportsWithTestNG;
@@ -27,12 +28,15 @@ public class TC_008_Iframe_New_Window extends BaseClass
 		Thread.sleep(4000);
 		we.findElement(By.xpath("//td[contains(text(),'QCEngineer4398')]")).click();
 		String parent=we.getWindowHandle();
-		windowHandles("apply_to_position");
-		elementSendText("xpath", Utility.fetchLocatorValue("email_field"), "test67554801@gmail.com");
+		windowHandles("apply_to_position");Random r1=new Random();
+		String emailName="QAEngineer";
+		emailName += Integer.toString(r1.nextInt(9999))+"@gmail.com";
+		System.out.println("Email name generated is:"+emailName);
+		elementSendText("xpath", Utility.fetchLocatorValue("email_field"), emailName);
 		elementClick("xpath",Utility.fetchLocatorValue("next_button_1"));
-		elementSendText("xpath", Utility.fetchLocatorValue("first_name_field"), "Teknas");
-		elementSendText("xpath", Utility.fetchLocatorValue("last_name_field"), "D");
-		elementSendText("xpath", Utility.fetchLocatorValue("phone_number_fied"), "9820353599");
+		elementSendText("xpath", Utility.fetchLocatorValue("first_name_field"),Utility.fetchLocatorValue("first_name_data"));
+		elementSendText("xpath", Utility.fetchLocatorValue("last_name_field"), Utility.fetchLocatorValue("last_name_data"));
+		elementSendText("xpath", Utility.fetchLocatorValue("phone_number_fied"), Utility.fetchLocatorValue("phone_number_data"));
 		elementClick("xpath", Utility.fetchLocatorValue("common_next_button"));
 		elementClick("xpath", Utility.fetchLocatorValue("common_next_button"));
 		elementClick("xpath", Utility.fetchLocatorValue("common_next_button"));
